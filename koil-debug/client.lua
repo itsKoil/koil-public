02304d2f841aee62e9b1ee34a6d79ad68dc9a5d4
koil <mitch.clout@gmail.com>
2018-07-13 10:59:20 +0200

    group hashes on peds

--- koil-debug/client.lua
+++ koil-debug/client.lua
@@ -2,20 +2,20 @@
 local dickheaddebug = false
 
 local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
 RegisterNetEvent("hud:enabledebug")
 AddEventHandler("hud:enabledebug",function()
-	dickheaddebug = not dickheaddebug
+    dickheaddebug = not dickheaddebug
     if dickheaddebug then
         print("Debug: Enabled")
     else
@@ -71,13 +71,13 @@ function GetVehicle()
             distanceFrom = distance
             rped = ped
            -- FreezeEntityPosition(ped, inFreeze)
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
-	    	end
+            if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
+                DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
+            else
+                DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
+            end
             if lowGrav then
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+5.0)
+                SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+5.0)
             end
         end
         success, ped = FindNextVehicle(handle)
@@ -100,20 +100,20 @@ function GetObject()
             distanceFrom = distance
             rped = ped
             --FreezeEntityPosition(ped, inFreeze)
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
-	    	end
-	    	zdicks = -1184516519
-	    	zdicks2 = GetEntityModel(ped)
-	    	if zdicks == zdicks2 then
-	    		--
-	    	end
+            if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
+                DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
+            else
+                DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
+            end
+            zdicks = -1184516519
+            zdicks2 = GetEntityModel(ped)
+            if zdicks == zdicks2 then
+                --
+            end
             if lowGrav then
-            	--ActivatePhysics(ped)
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
-            	FreezeEntityPosition(ped, false)
+                --ActivatePhysics(ped)
+                SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
+                FreezeEntityPosition(ped, false)
             end
         end
 
@@ -140,16 +140,16 @@ function getNPC()
             distanceFrom = distance
             rped = ped
 
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
-	    	end
+            if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
+                DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship HASH: " .. GetPedRelationshipGroupHash(ped) .. " IN CONTACT" )
+            else
+                DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship HASH: " .. GetPedRelationshipGroupHash(ped) )
+            end
 
             FreezeEntityPosition(ped, inFreeze)
             if lowGrav then
-            	SetPedToRagdoll(ped, 511, 511, 0, 0, 0, 0)
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
+                SetPedToRagdoll(ped, 511, 511, 0, 0, 0, 0)
+                SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
             end
         end
         success, ped = FindNextPed(handle)
