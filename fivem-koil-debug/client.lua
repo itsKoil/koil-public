7c30e09d2565740a08580a145aef84126e54e93c
AvarianKnight <dillskaggs@gmail.com>
2020-10-20 03:16:01 -0500

    optimize

--- fivem-koil-debug/client.lua
+++ fivem-koil-debug/client.lua
@@ -1,5 +1,5 @@
 
-local dickheaddebug = false
+local debugEnabled = false
 
 local Keys = {
 	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
@@ -15,8 +15,8 @@ local Keys = {
 
 RegisterNetEvent("hud:enabledebug")
 AddEventHandler("hud:enabledebug",function()
-	dickheaddebug = not dickheaddebug
-    if dickheaddebug then
+	debugEnabled = not debugEnabled
+    if debugEnabled then
         print("Debug: Enabled")
     else
         print("Debug: Disabled")
@@ -40,167 +40,203 @@ function drawTxt(x,y ,width,height,scale, text, r,g,b,a)
     DrawText(x - width/2, y - height/2 + 0.005)
 end
 
-
-function DrawText3Ds(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
-    local factor = (string.len(text)) / 370
-    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+function DrawText3Ds(coords, text)
+    local onScreen,_x,_y=World3dToScreen2d(coords.x, coords.y, coords.z)
+    -- local px,py,pz=table.unpack(GetGameplayCamCoords())
+	
+	if onScreen then
+		SetTextScale(0.35, 0.35)
+		SetTextFont(4)
+		SetTextProportional(1)
+		SetTextColour(255, 255, 255, 215)
+		SetTextEntry("STRING")
+		SetTextCentre(1)
+		AddTextComponentString(text)
+		DrawText(_x,_y)
+		-- local factor = (string.len(text)) * 0.0025
+		-- DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
+	end
 end
 
-function GetVehicle()
-    local playerped = GetPlayerPed(-1)
-    local playerCoords = GetEntityCoords(playerped)
-    local handle, ped = FindFirstVehicle()
-    local success
-    local rped = nil
-    local distanceFrom
-    repeat
-        local pos = GetEntityCoords(ped)
-        local distance = GetDistanceBetweenCoords(playerCoords, pos, true)
-        if canPedBeUsed(ped) and distance < 30.0 and (distanceFrom == nil or distance < distanceFrom) then
-            distanceFrom = distance
-            rped = ped
-           -- FreezeEntityPosition(ped, inFreeze)
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Veh: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
-	    	end
-            if lowGrav then
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+5.0)
-            end
-        end
-        success, ped = FindNextVehicle(handle)
-    until not success
-    EndFindVehicle(handle)
-    return rped
+local objTable = {}
+
+function updateObjTable()
+	objTable = {}
+
+	for obj in EnumerateObjects() do
+		if obj ~= 0 then
+			objTable[#objTable+1] = obj
+		end
+	end
 end
 
-function GetObject()
-    local playerped = GetPlayerPed(-1)
-    local playerCoords = GetEntityCoords(playerped)
-    local handle, ped = FindFirstObject()
-    local success
-    local rped = nil
-    local distanceFrom
-    repeat
-        local pos = GetEntityCoords(ped)
-        local distance = GetDistanceBetweenCoords(playerCoords, pos, true)
-        if distance < 10.0 then
-            distanceFrom = distance
-            rped = ped
-            --FreezeEntityPosition(ped, inFreeze)
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"]+1, "Obj: " .. ped .. " Model: " .. GetEntityModel(ped) .. "" )
-	    	end
+local vehTable = {}
 
-            if lowGrav then
-            	--ActivatePhysics(ped)
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
-            	FreezeEntityPosition(ped, false)
-            end
+function updateVehTable()
+	vehTable = {}
+	for veh in EnumerateVehicles() do
+		if veh ~= 0 then
+			vehTable[#vehTable+1] = veh
+		end
+	end
+end
+
+local pedTable = {}
+
+function updatePedTable()
+	pedTable = {}
+	for ped in EnumeratePeds() do
+		if ped ~= 0 and ped ~= PlayerPedId() then
+			pedTable[#pedTable+1] = ped
+		end
+	end
+end
+
+
+local entityEnumerator = {
+    __gc = function(enum)
+        if enum.destructor and enum.handle then
+            enum.destructor(enum.handle)
         end
 
-        success, ped = FindNextObject(handle)
-    until not success
-    EndFindObject(handle)
-    return rped
-end
+        enum.destructor = nil
+        enum.handle = nil
+    end
+}
 
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+    return coroutine.wrap(function()
+        local iter, id = initFunc()
 
+        if not id or id == 0 then
+            disposeFunc(iter)
 
-
-function getNPC()
-    local playerped = GetPlayerPed(-1)
-    local playerCoords = GetEntityCoords(playerped)
-    local handle, ped = FindFirstPed()
-    local success
-    local rped = nil
-    local distanceFrom
-    repeat
-        local pos = GetEntityCoords(ped)
-        local distance = GetDistanceBetweenCoords(playerCoords, pos, true)
-        if canPedBeUsed(ped) and distance < 30.0 and (distanceFrom == nil or distance < distanceFrom) then
-            distanceFrom = distance
-            rped = ped
-
-	    	if IsEntityTouchingEntity(GetPlayerPed(-1), ped) then
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship HASH: " .. GetPedRelationshipGroupHash(ped) .. " IN CONTACT" )
-	    	else
-	    		DrawText3Ds(pos["x"],pos["y"],pos["z"], "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship HASH: " .. GetPedRelationshipGroupHash(ped) )
-	    	end
-
-            FreezeEntityPosition(ped, inFreeze)
-            if lowGrav then
-            	SetPedToRagdoll(ped, 511, 511, 0, 0, 0, 0)
-            	SetEntityCoords(ped,pos["x"],pos["y"],pos["z"]+0.1)
-            end
+            return
         end
-        success, ped = FindNextPed(handle)
-    until not success
-    EndFindPed(handle)
-    return rped
+
+        local enum = {
+            handle = iter,
+            destructor = disposeFunc
+        }
+
+        setmetatable(enum, entityEnumerator)
+        local next = true
+        repeat
+            coroutine.yield(id)
+            next, id = moveFunc(iter)
+        until not next
+        enum.destructor, enum.handle = nil, nil
+        disposeFunc(iter)
+    end)
 end
 
-function canPedBeUsed(ped)
-    if ped == nil then
-        return false
-    end
-    if ped == GetPlayerPed(-1) then
-        return false
-    end
-    if not DoesEntityExist(ped) then
-        return false
-    end
-    return true
+function EnumerateObjects()
+    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
 end
 
+function EnumeratePeds()
+    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+function EnumerateVehicles()
+    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+function EnumeratePickups()
+    return EnumerateEntities(FindFirstPickup, FindNextPickup, EndFindPickup)
+end
+
+local relationships = {
+	[GetHashKey("PLAYER")] = "PLAYER",
+	[GetHashKey("CIVMALE")] = "CIVMALE",
+	[GetHashKey("CIVFEMALE")] = "CIVFEMALE",
+	[GetHashKey("COP")] = "COP",
+	[GetHashKey("SECURITY_GUARD")] = "SECURITY_GUARD",
+	[GetHashKey("PRIVATE_SECURITY")] = "PRIVATE_SECURITY",
+	[GetHashKey("FIREMAN")] = "FIREMAN",
+	[GetHashKey("GANG_1")] = "GANG_1",
+	[GetHashKey("GANG_2")] = "GANG_2",
+	[GetHashKey("GANG_9")] = "GANG_9",
+	[GetHashKey("GANG_10")] = "GANG_10",
+	[GetHashKey("AMBIENT_GANG_LOST")] = "AMBIENT_GANG_LOST",
+	[GetHashKey("AMBIENT_GANG_MEXICAN")] = "AMBIENT_GANG_MEXICAN",
+	[GetHashKey("AMBIENT_GANG_FAMILY")] = "AMBIENT_GANG_FAMILY",
+	[GetHashKey("AMBIENT_GANG_BALLAS")] = "AMBIENT_GANG_BALLAS",
+	[GetHashKey("AMBIENT_GANG_MARABUNTE")] = "AMBIENT_GANG_MARABUNTE",
+	[GetHashKey("AMBIENT_GANG_CULT")] = "AMBIENT_GANG_CULT",
+	[GetHashKey("AMBIENT_GANG_SALVA")] = "AMBIENT_GANG_SALVA",
+	[GetHashKey("AMBIENT_GANG_WEICHENG")] = "AMBIENT_GANG_WEICHENG",
+	[GetHashKey("AMBIENT_GANG_HILLBILLY")] = "AMBIENT_GANG_HILLBILLY",
+	[GetHashKey("DEALER")] = "DEALER",
+	[GetHashKey("HATES_PLAYER")] = "HATES_PLAYER",
+	[GetHashKey("HEN")] = "HEN",
+	[GetHashKey("WILD_ANIMAL")] = "WILD_ANIMAL",
+	[GetHashKey("SHARK")] = "SHARK",
+	[GetHashKey("COUGAR")] = "COUGAR",
+	[GetHashKey("NO_RELATIONSHIP")] = "NO_RELATIONSHIP",
+	[GetHashKey("SPECIAL")] = "SPECIAL",
+	[GetHashKey("MISSION2")] = "MISSION2",
+	[GetHashKey("MISSION3")] = "MISSION3",
+	[GetHashKey("MISSION4")] = "MISSION4",
+	[GetHashKey("MISSION5")] = "MISSION5",
+	[GetHashKey("MISSION6")] = "MISSION6",
+	[GetHashKey("MISSION7")] = "MISSION7",
+	[GetHashKey("MISSION8")] = "MISSION8",
+	[GetHashKey("ARMY")] = "ARMY",
+	[GetHashKey("GUARD_DOG")] = "GUARD_DOG",
+	[GetHashKey("AGGRESSIVE_INVESTIGATE")] = "AGGRESSIVE_INVESTIGATE",
+	[GetHashKey("MEDIC")] = "MEDIC",
+	[GetHashKey("CAT")] = "CAT",
+}
 
 
+local currentStreetName = ""
+
+local lastTableUpdate = 0
 Citizen.CreateThread( function()
 
     while true do 
         
-        Citizen.Wait(1)
+        Citizen.Wait(0)
         
-        if dickheaddebug then
-            local pos = GetEntityCoords(GetPlayerPed(-1))
+		if true then
+			local ply = PlayerPedId()
+            local pos = GetEntityCoords(ply)
 
-            local forPos = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 0, 1.0, 0.0)
-            local backPos = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 0, -1.0, 0.0)
-            local LPos = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 1.0, 0.0, 0.0)
-            local RPos = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), -1.0, 0.0, 0.0) 
+            local forPos = GetOffsetFromEntityInWorldCoords(ply, 0, 1.0, 0.0)
+            local backPos = GetOffsetFromEntityInWorldCoords(ply, 0, -1.0, 0.0)
+            local LPos = GetOffsetFromEntityInWorldCoords(ply, 1.0, 0.0, 0.0)
+            local RPos = GetOffsetFromEntityInWorldCoords(ply, -1.0, 0.0, 0.0) 
 
-            local forPos2 = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 0, 2.0, 0.0)
-            local backPos2 = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 0, -2.0, 0.0)
-            local LPos2 = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), 2.0, 0.0, 0.0)
-            local RPos2 = GetOffsetFromEntityInWorldCoords(GetPlayerPed(-1), -2.0, 0.0, 0.0)    
+            local forPos2 = GetOffsetFromEntityInWorldCoords(ply, 0, 2.0, 0.0)
+            local backPos2 = GetOffsetFromEntityInWorldCoords(ply, 0, -2.0, 0.0)
+            local LPos2 = GetOffsetFromEntityInWorldCoords(ply, 2.0, 0.0, 0.0)
+            local RPos2 = GetOffsetFromEntityInWorldCoords(ply, -2.0, 0.0, 0.0)    
 
-            local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-            local currentStreetHash, intersectStreetHash = GetStreetNameAtCoord(x, y, z, currentStreetHash, intersectStreetHash)
-            currentStreetName = GetStreetNameFromHashKey(currentStreetHash)
+			local x, y, z = table.unpack(GetEntityCoords(ply, true))
+			
+			
+			local plyHeading = GetEntityHeading(ply)
+			local attachedEnt = GetEntityAttachedTo(ply)
+			local plyHealth = GetEntityHealth(ply)
+			local hag = GetEntityHeightAboveGround(ply)
+			local plyModel = GetEntityModel(ply)
+			local speed = GetEntitySpeed(ply)
 
-            drawTxt(0.8, 0.50, 0.4,0.4,0.30, "Heading: " .. GetEntityHeading(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.52, 0.4,0.4,0.30, "Coords: " .. pos, 55, 155, 55, 255)
-            drawTxt(0.8, 0.54, 0.4,0.4,0.30, "Attached Ent: " .. GetEntityAttachedTo(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.56, 0.4,0.4,0.30, "Health: " .. GetEntityHealth(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.58, 0.4,0.4,0.30, "H a G: " .. GetEntityHeightAboveGround(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.60, 0.4,0.4,0.30, "Model: " .. GetEntityModel(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.62, 0.4,0.4,0.30, "Speed: " .. GetEntitySpeed(GetPlayerPed(-1)), 55, 155, 55, 255)
-            drawTxt(0.8, 0.64, 0.4,0.4,0.30, "Frame Time: " .. GetFrameTime(), 55, 155, 55, 255)
-            drawTxt(0.8, 0.66, 0.4,0.4,0.30, "Street: " .. currentStreetName, 55, 155, 55, 255)
+			-- tried to make this readable, don't think it worked.
+			drawTxt(0.8, 0.50, 0.4,0.4,0.30, 
+				"\nHeading: " .. plyHeading ..
+				"\nCoords: " .. pos ..
+				"\nAttached Ent: " .. attachedEnt
+				, 55, 155, 55, 255)
+			drawTxt(0.8, 0.55, 0.4,0.4,0.30, 
+				"\nHealth: " .. plyHealth ..
+				"\nH a G: " .. hag .. 
+				"\nModel: " .. plyModel ..
+				"\nSpeed: " .. speed, 55, 155, 55, 255)
+			drawTxt(0.8, 0.615, 0.4,0.4,0.30, 
+				"\nFrame Time: " .. GetFrameTime() ..
+				"\nStreet: " .. currentStreetName, 55, 155, 55, 255)
             
             
             DrawLine(pos,forPos, 255,0,0,115)
@@ -215,11 +251,78 @@ Citizen.CreateThread( function()
             DrawLine(LPos,LPos2, 255,255,255,115)
             DrawLine(RPos,RPos2, 255,255,255,115)     
 
-            local nearped = getNPC()
+			if lastTableUpdate < GetGameTimer() then
+				-- print("update tables")
+				updatePedTable()
+				updateVehTable()
+				updateObjTable()
 
-            local veh = GetVehicle()
+				local currentStreetHash, intersectStreetHash = GetStreetNameAtCoord(pos.x, pos.y, pos.z, currentStreetHash, intersectStreetHash)
+				currentStreetName = GetStreetNameFromHashKey(currentStreetHash)
+				lastTableUpdate = GetGameTimer() + 1000
+			end
+			
+			for i = 1, #pedTable do
+				local ped = pedTable[i]
+				local pedCoords = GetEntityCoords(ped)
+				local plyPed = PlayerPedId()
+				local plyCoords = GetEntityCoords(plyPed)
 
-            local nearobj = GetObject()
+				if #(pedCoords - plyCoords) < 15.0 then
+					if IsEntityTouchingEntity(plyPed, ped) then
+						DrawText3Ds(pedCoords, "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship: " .. relationships[GetPedRelationshipGroupHash(ped)] .. " TOUCHING" )
+					else
+						DrawText3Ds(pedCoords, "Ped: " .. ped .. " Model: " .. GetEntityModel(ped) .. " Relationship: " .. relationships[GetPedRelationshipGroupHash(ped)] )
+					end
+				end
+
+				if lowGrav then
+					SetPedToRagdoll(ped, 511, 511, 0, 0, 0, 0)
+					SetEntityCoords(ped, pedCoords + vector3(0, 0, 0.1))
+				end
+				FreezeEntityPosition(ped, inFreeze)
+
+			end
+
+			for i = 1, #objTable do
+				local obj = objTable[i]
+				local objCoords = GetEntityCoords(obj)
+				local plyPed = PlayerPedId()
+				local plyCoords = GetEntityCoords(plyPed)
+
+				if #(objCoords - plyCoords) < 15.0 then
+					if IsEntityTouchingEntity(plyPed, obj) then
+						DrawText3Ds(objCoords + vector3(0,0,1), "Obj: " .. obj .. " Model: " .. GetEntityModel(obj) .. " IN CONTACT" )
+					else
+						DrawText3Ds(objCoords + vector3(0,0,1), "Obj: " .. obj .. " Model: " .. GetEntityModel(obj) .. "" )
+					end
+				end
+
+				if lowGrav then
+					SetEntityCoords(obj, objCoords + vector3(0,0,0.1))
+					FreezeEntityPosition(obj, false)
+				end
+				FreezeEntityPosition(obj, inFreeze)
+			end
+
+			for i = 1, #vehTable do
+				local veh = vehTable[i]
+				local vehCoords = GetEntityCoords(veh)
+				local plyPed = PlayerPedId()
+				local plyCoords = GetEntityCoords(plyPed)
+
+				if #(vehCoords - plyCoords) < 15.0 then
+					if IsEntityTouchingEntity(plyPed, ped) then
+						DrawText3Ds(vehCoords + vector3(0,0,1), "Veh: " .. veh .. " Model: " .. GetDisplayNameFromVehicleModel(GetEntityModel(veh)) .. " IN CONTACT" )
+					else
+						DrawText3Ds(vehCoords + vector3(0,0,1), "Veh: " .. veh .. " Model: " .. GetDisplayNameFromVehicleModel(GetEntityModel(veh)) .. "" )
+					end
+				end
+				if lowGrav then
+					SetEntityCoords(veh,vehCoords + vector3(0,0,5))
+				end
+				FreezeEntityPosition(veh, inFreeze)
+			end
 
             if IsControlJustReleased(0, 38) then
                 if inFreeze then
@@ -242,7 +345,7 @@ Citizen.CreateThread( function()
             end
 
         else
-            Citizen.Wait(5000)
+            Citizen.Wait(500)
         end
     end
 end)
---
+++ fivem-koil-debug/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_scripts {
+	'server.lua',
+}
+client_script {
+	'client.lua',
+}
